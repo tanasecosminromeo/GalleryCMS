f5e4d91f4d1b535399f8d0bf4b5845fafe071390
weboAp <mdouaou@gmail.com>
2011-05-26 00:04:02 -0400

    added common class Gcmsadmin_controller to MY_Controlller and extended gcmsadmin controller to it

--- application/modules/gcmsadmin/controllers/gcmsadmin.php
+++ application/modules/gcmsadmin/controllers/gcmsadmin.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Gcmsadmin extends Admin_Controller {
+class Gcmsadmin extends Gcmsadmin_Controller {
 
 	/**
 	 * Index Page for this controller.
